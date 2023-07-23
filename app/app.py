50671b8e06c789447228b5293fccc18570bc3858
maijun <maijun2@gmail.com>
2023-07-23 00:51:18 +0000

    edit app.py

--- app/app.py
+++ app/app.py
@@ -1,16 +1,21 @@
-import os, flask, datetime, redis
+import os, flask, datetime, redis, pytz
 from flask import Flask
 from flask import render_template
 from redis import StrictRedis
 from datetime import datetime
+from pytz import timezone
 
 PORT = int(os.environ['PORT'])
 app = flask.Flask('app server')
-redis = StrictRedis(host='redis',port=6379)
+HOST = os.environ['HOST']
+redis = StrictRedis(host=HOST,port=6379)
+
 
 @app.route('/')
 def index():
-    redis.lpush('times',datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+    utc_now = datetime.now(timezone('UTC'))
+    jst_now = utc_now.astimezone(timezone('Asia/Tokyo')).strftime("%Y/%m/%d %H:%M:%S")
+    redis.lpush('times',jst_now)
     return render_template('index.html', title='動作確認',times=redis.lrange('times', 0, -1))
     
 app.run(debug=True, host='0.0.0.0',port=PORT)
